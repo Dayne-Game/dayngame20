a72617359a0a3c2321104a3801fc9ad34bdd0482
Dayne Game <daynegame024@gmail.com>
2017-08-03 16:17:16 +1200

    My Notes

--- Program.cs
+++ Program.cs
@@ -6,7 +6,7 @@ namespace dayngame20
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello Worl
+            Console.WriteLine("Hello World");
         }
     }
 }
